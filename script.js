3bd96c80dc7fd1437ce87417c28207f04d71d4b5
Donauer <35851756+adambaranec@users.noreply.github.com>
2026-08-10 13:03:21 +0200

    Refactor work info display in script.js
    Updated HTML structure for work information display.

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const works = [
 ];
 
 /*
+if (!works.length){
 container.innerHTML = works.map(item => `
   <div class="card">
     <img src=${item.Image}></img>
-    <p><strong>${item.Name}</strong></p>
-    <p><${item.Author}</p>
-    <p>${item.Country}</p>
-    <p>${item.Date}</p>
+    <p class="workInfo"><strong>${item.Name}</strong></p>
+    <p class="workInfo"><${item.Author}</p>
+    <p class="workInfo">${item.Country}</p>
+    <p class="workInfo">${item.Date}</p>
   </div>
 `).join('');
+}
 */
 
 document.getElementById('copyright-year').textContent = new Date().getFullYear();
