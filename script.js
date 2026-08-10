f2abdc0d30f9d81eaa5ccb2bbe7275b08203f528
Adam Baranec <abaranec@gmail.com>
2026-08-10 16:31:56 +0200

    changed css to contain artworks

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const works = [
-  
+
 ];
 
-/*
-if (!works.length){
-container.innerHTML = works.map(item => `
-  <div class="card">
-    <img src=${item.Image}></img>
-    <p class="workInfo"><strong>${item.Name}</strong></p>
-    <p class="workInfo"><${item.Author}</p>
-    <p class="workInfo">${item.Country}</p>
-    <p class="workInfo">${item.Date}</p>
-  </div>
-`).join('');
+const container = document.querySelector('.container');
+
+if (works.length != 0){
+const newHTML = works.map(item => `
+    <div class="card">
+      <img src="${item.image}" />
+      <p class="workInfo workName"><strong>${item.name}</strong></p>
+      <p class="workInfo">${item.author}</p>
+      <p class="workInfo">${item.country}</p>
+      <p class="workInfo">${item.year}</p>
+    </div>
+  `).join('');
+container.innerHTML = newHTML;
 }
-*/
 
 document.getElementById('copyright-year').textContent = new Date().getFullYear();
 
