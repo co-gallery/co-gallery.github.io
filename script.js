8322b34c239d464cce0f68ab80b051f3de665281
Donauer <35851756+adambaranec@users.noreply.github.com>
2026-08-10 09:12:43 +0200

    Comment out HTML generation code
    Some updates

--- script.js
+++ script.js
@@ -2,5 +2,16 @@ const works = [
   
 ];
 
+/*
+container.innerHTML = jsonData.map(item => `
+  <div class="card">
+    <h2>${item.Name}</h2>
+    <p><${item.Author}</p>
+    <p>${item.Country}</p>
+    <p>${item.Date}</p>
+  </div>
+`).join('');
+*/
+
 document.getElementById('copyright-year').textContent = new Date().getFullYear();
 
